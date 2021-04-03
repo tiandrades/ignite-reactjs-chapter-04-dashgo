59fb09159766871dd7635860eaad51effb48e7ed
Tiago Andrade <tailergamer7@gmail.com>
2021-04-03 12:24:31 -0400

    🔨 refactor(lesson: Header responsivo): update header component

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,14 +1,20 @@
 import { Avatar, Box, Flex, Text, } from '@chakra-ui/react';
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({ showProfileData = true }: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Tiago Andrade</Text>
-        <Text color="gray.300" fontSize="small">
-          myemail@domain
-      </Text>
-      </Box>
+      { showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Tiago Andrade</Text>
+          <Text color="gray.300" fontSize="small">
+            myemail@domain
+              </Text>
+        </Box>
+      )}
 
       <Avatar size="md" name="Tiago Andrade" src="https://github.com/tiagooandrade.png" />
     </Flex>
