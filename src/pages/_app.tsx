d432885115d07e5cc4f08f379e6d754ac05f73d3
Tiago Andrade <tailergamer7@gmail.com>
2021-04-09 13:24:53 -0400

    :bulb: chore(lesson: Configurando MirageJS): add miragejs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,6 +3,11 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { theme } from '../styles/theme';
 
 import { SidebarDrawerProvider } from '../contexts/SideBarDrawerContenxt';
+import { makeServer } from '../services/mirage';
+
+if (process.env.NODE_ENV === 'development') {
+  makeServer();
+}
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
